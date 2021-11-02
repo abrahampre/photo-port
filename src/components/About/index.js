dcf1ee8b5875af977512e77bb4c0d793b818fe6f
abraham perez reyes <abrahampre27@gmail.com>
2021-11-01 21:23:56 -0500

    added components about and nav

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import coverImage from "../../assets/cover/cover-image.jpg"
+
+function About (){
+    return(
+        <section className = "my-5">
+            <hi id="about">Who am I</hi>
+            <img src={coverImage} className = "my-2" style={{width:"100%"}} alt = "cover"/>
+        </section>
+    );
+}
+export default About;
